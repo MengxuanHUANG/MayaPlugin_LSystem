559964af1be7ace4ce9551305ae6cc0c03ec9693
MengxuanHUANG <53021701+MengxuanHUANG@users.noreply.github.com>
2023-01-23 21:46:25 -0500

    add extrude command

--- LSystemCmd.cpp
+++ LSystemCmd.cpp
@@ -18,8 +18,12 @@ MStatus LSystemCmd::doIt( const MArgList& args )
 	std::cout.flush();
 
 	// message in scriptor editor
-	MGlobal::displayInfo("Implement Me!");
-
-    return MStatus::kSuccess;
+	MString extrude = R"(curve -d 1 -p 0 0 0 - p 1 1 1 -k 0 - k 1 - name curve1;
+						 select - r ^1s curve1;
+						 extrude - ch true - rn false - po 1 - et 2 - ucp 1 - fpt 1 - upn 1 - rotation 0 - scale 1 - rsp 1 "nurbsCircle1" "curve1"; )";
+	MString command;
+	command.format(extrude, "nurbsCircle1");
+	MStatus status = MGlobal::executeCommand(command);
+	return status;
 }
 
